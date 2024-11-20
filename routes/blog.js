9428230594c77e23e5a35bf447de5f0c96fc70ca
Sarthak Madhamshettiwar <sarthakmadhamshettiwar@gmail.com>
2024-11-20 23:40:56 +0530

    Update blog.js
    Included bullMQ for task queue

--- routes/blog.js
+++ routes/blog.js
@@ -4,9 +4,28 @@ const Redis = require('redis');
 const path = require('path'); // Added path module import
 const Blog = require('../models/blog');
 const Comment = require('../models/comment');   //Each blog will have comments associated with it
+
+//Notification Setup using BullMQ
+
+const {Queue} = require("bullmq");
+const notificationQueue = new Queue('email_queue');
+
+async function addToQueue(title, blogId, email) {
+  text = 'Congratulations! Your blog '+ title + 'has been successfully published! You can view it at http://localhost:8000/blog/'+blogId
+  const result = await notificationQueue.add('email user',{
+    'from': 'iit2021223@iiita.ac.in',
+    'to': email,
+    'subject': `Blog published`,
+    'text': text,
+  });
+  //console.log('Job added to queue ', result.id);
+}
+
+
+
+
 const DEFAULT_EXPIRATION = 3600;
 const router = express.Router();
-
 
 imagesPath = path.join(__dirname, '..', 'public');
 router.use(express.static(imagesPath));
@@ -33,10 +52,71 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+router.post("/", upload.single('coverImage'), async (req, res) => {
+    const {title, body} = req.body;
+    const blog = await Blog.create({
+        body, 
+        title,
+        createdBy: req.user._id,
+        coverImageURL: `uploads/${req.file.filename}`,
+    });
+    //console.log(req.user);
+    //send email here about blog getting published
+    try {
+        addToQueue(title, blog._id, req.user.email);
+    }
+    catch(err){
+        console.log('Error: ', err);
+    }
+    return res.redirect(`/blog/${blog._id}`);
+});
+
+router.post("/comment/:blogId", async (req, res)=>{
+    //console.log(req.body.content)
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    });
+
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+//Working perfectly fine after deleteing DB and creating again followed by indexing on 'title'
+router.get("/search/:id", async (req, res) => {
+    try {
+        const query = req.params.id;  // Get the query parameter
+        //console.log(query);
+        if (!query) {
+            return res.status(400).json({ message: "Query parameter is required" });
+        }
+        console.log("Search query:", query);
+
+        // Make sure the search query is sanitized and valid
+        const searchResult = await Blog.find({ $text: { $search: query } });
+        //console.log("Search result:", searchResult);
+        
+        // return res.render('/searchedBlogs', {query:query, user: req.user, blogs: searchResult});
+        return res.render('searchedBlogs', { user: req.user, query:req.params.id, blogs:searchResult });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+
+router.get("/search/:query", async (req, res)=>{
+    //const blogs = await Blog.find({$text:{$search:req.params.query}});
+    //console.log(req.param.query);
+    const blogs = {'title':'Bello'};
+    //console.log("hllo");
+    //res.render('searchedBlogs',{searchQuery: req.params.query, blogs:blogs, user:req.user});
+    res.send('Hello Worl');
+})
+
 router.get("/add-new", (req, res) => {
     return res.render('addBlog', { user: req.user });
 });
-
 
 router.get("/:id", async (req, res) => {
     const blogKey = `blog:${req.params.id}`;
@@ -73,7 +153,6 @@ router.get("/:id", async (req, res) => {
                     }
                 });
                 //console.log('Cache miss');
-                console.log('Cache miss');
                 res.render('blog', { user: req.user, blog, comments });
             }
         }
